795acfec6f097bab875724eb267cffa2b79762e3
StefanLederer <stefan.lederer18@gmail.com>
2017-05-21 20:16:56 +0200

    created main menu design

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,15 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * Created by stefanlederer on 19.05.17.
@@ -19,8 +29,35 @@ public class Application {
         mainFrame.setLocation((int)(Toolkit.getDefaultToolkit().getScreenSize().getWidth()/2 - mainFrameWidth/2), (int)(Toolkit.getDefaultToolkit().getScreenSize().getHeight()/2 - mainFrameHeight/2));
 
         JPanel menuPanel = new JPanel();
-        mainFrame.add(menuPanel);
-        mainFrame.getContentPane().add(menuPanel, BorderLayout.LINE_START);
+        menuPanel.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK));
+        mainFrame.add(menuPanel, BorderLayout.PAGE_START);
+
+        BufferedImage plusIcon = null;
+        try {
+            plusIcon = ImageIO.read(ClassLoader.getSystemResource("icons/plus.png"));
+        } catch(IOException e) {
+            System.out.println("add Image error: " + e);
+        }
+        JLabel addButton = new JLabel(new ImageIcon(plusIcon));
+        addButton.setBorder(BorderFactory.createEmptyBorder());
+        addButton.setPreferredSize(new Dimension(20, 20));
+        addButton.setLocation(10, 10);
+        menuPanel.add(addButton);
+
+        addButton.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("add Button Clicked");
+            }
+        });
+
+        JButton compressButton = new JButton("Komprimieren");
+        menuPanel.add(compressButton);
+
+        compressButton.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("compress Button Clicked");
+            }
+        });
 
 
         mainFrame.pack();
