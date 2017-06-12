352aa5e49ce0264eeb36cb45f4156f6e0f6105d6
StefanLederer <stefan.lederer18@gmail.com>
2017-06-12 17:58:56 +0200

    created compressed pixel list for body statement

--- src/main/java/Analysis.java
+++ src/main/java/Analysis.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
@@ -7,8 +8,10 @@ public class Analysis {
 
     public static void analysePixels(String[][] pixels) {
         ArrayList<String> headerSegment = new ArrayList<String>();
+        ArrayList<String> compressedPixel = new ArrayList<String>();
 
         boolean equal = false;
+        int counter = 0;
         for (int width = 0; width < pixels.length; width++) {
             for (int height = 0; height < pixels[width].length; height++) {
                 String segment = pixels[width][height].substring(0, 1) + pixels[width][height].substring(2, 3) + pixels[width][height].substring(4, 5);
@@ -22,15 +25,24 @@ public class Analysis {
                 }
 
                 if (!equal) {
+                    counter++;
                     headerSegment.add(segment);
                 } else {
                     equal = false;
                 }
-
+                String split;
+                    if (width == 0) {
+                        split = ";";
+                    } else {
+                        split = "";
+                    }
+                compressedPixel.add(split + counter + "-" + deviation);
 
             }
         }
         System.out.println(headerSegment);
+        System.out.println(compressedPixel.size());
+        System.out.println(compressedPixel.get(0));
 
     }
 }
