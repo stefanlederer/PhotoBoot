3e65879f29e24e47595e466bfb67e4c34a5e0583
StefanLederer <stefan.lederer18@gmail.com>
2017-06-12 17:12:41 +0200

    fixed hexcode analyse + created header segment list

--- src/main/java/Analysis.java
+++ src/main/java/Analysis.java
@@ -6,33 +6,31 @@ import java.util.ArrayList;
 public class Analysis {
 
     public static void analysePixels(String[][] pixels) {
-        ArrayList<ArrayList<String>> valence = new ArrayList<ArrayList<String>>();
-        for (int i = 0; i < 3; i++) {
-            valence.add(new ArrayList<String>());
-        }
+        ArrayList<String> headerSegment = new ArrayList<String>();
+
         boolean equal = false;
         for (int width = 0; width < pixels.length; width++) {
             for (int height = 0; height < pixels[width].length; height++) {
-                for (int priority = 0; priority < 3; priority++) {
-                    if (valence.get(priority).size() > 0) {
-                        for (int similarity = 0; similarity < valence.get(priority).size(); similarity++) {
-                            if (valence.get(priority).get(similarity).equals(pixels[width][height].substring(0, priority + 1))) {
-                                equal = true;
-                            }
+                String segment = pixels[width][height].substring(0, 1) + pixels[width][height].substring(2, 3) + pixels[width][height].substring(4, 5);
+                String deviation = pixels[width][height].substring(1, 2) + pixels[width][height].substring(3, 4) + pixels[width][height].substring(5, 6);
+                if (headerSegment.size() > 0) {
+                    for (int similarity = 0; similarity < headerSegment.size(); similarity++) {
+                        if (headerSegment.get(similarity).equals(segment)) {
+                            equal = true;
                         }
                     }
+                }
 
-                    if (!equal) {
-                        valence.get(priority).add(pixels[width][height].substring(0, priority + 1));
-                    } else {
-                        equal = false;
-                    }
+                if (!equal) {
+                    headerSegment.add(segment);
+                } else {
+                    equal = false;
                 }
 
 
             }
         }
-        System.out.println(valence);
+        System.out.println(headerSegment);
 
     }
 }
